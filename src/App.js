6f326d792939fdf0d13b2fd6b4971bef5c1c7f77
i-one7 <ridhoang93@gmail.com>
2022-09-29 03:37:28 +0700

    add chakra provider to app and remove some trash at index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import Main from './content/main.content';
+import themes from './theme/theme';
 
 const App = () => {
 	return (
-		<>
-		</>
-	)
-}
+		<ChakraProvider theme={themes}>
+			<Main/>
+		</ChakraProvider>
+	);
+};
 
 export default App;
