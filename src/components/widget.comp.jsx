568d7a1e6a18175ae932dc97596972bf58da57cf
i-one7 <ridhoang93@gmail.com>
2022-09-30 05:59:47 +0700

    change padding and outlines

--- src/components/widget.comp.jsx
+++ src/components/widget.comp.jsx
@@ -2,14 +2,12 @@ import { Box } from '@chakra-ui/react';
 
 const config = {
 	widget: {
-		outline: '1.4px solid',
-		outlineColor: 'dark.600',
 		overflow: 'hidden',
 		bg: 'dark.700',
 		rounded: 'lg',
 		w: 'full',
 		py: 6,
-		px: 2,
+		px: 0,
 	},
 };
 
