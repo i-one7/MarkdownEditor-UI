cadde832d3ab6b4d9d2c484f92c718edf8325fc4
i-one7 <ridhoang93@gmail.com>
2022-09-30 06:04:28 +0700

    change hex color & override some chakra ui built-in component styles

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -20,10 +20,10 @@ const config = {
 };
 
 const dark = {
-	600: '#272c35',
-	700: '#1a1f28',
-	800: '#121417',
-	900: '#090b0d',
+	600: '#65737d',
+	700: '#25282f',
+	800: '#1c1f24',
+	900: '#0e1118',
 };
 
 const Textarea = {
@@ -37,10 +37,10 @@ const Textarea = {
 			px: 4,
 			py: 0,
 			_focus: {
-				boxShadow: 'none'
+				boxShadow: 'none',
 			},
 			'::-webkit-scrollbar': {
-				display: 'none'
+				display: 'none',
 			},
 		},
 	},
@@ -53,25 +53,58 @@ const Tabs = {
 			px: 4
 		},
 		tab: {
+			border: '1.6px dashed transparent',
 			fontWeight: 'semibold',
 			roundedTop: 'md',
 			borderBottom: 0,
 			_selected: {
+				borderColor: 'dark.600',
 				bg: 'dark.800',
 			}
 		},
 		tabpanel: {
+			borderY: '1.6px dashed',
+			borderColor: 'dark.600',
 			bg: 'dark.800',
-			rounded: 'sm',
 			py: 6,
 			p: 0,
 		},
 	},
 };
 
+const Divider = {
+	variants: {
+		solid: {
+			rounded: 'full',
+			borderWidth: '2.2px',
+			my: 3,
+		},
+	},
+};
+
+const Button = {
+	baseStyle: {
+		fontWeight: 'normal',
+		outline: 'none',
+		rounded: 'full',
+		ml: 0,
+		mr: 0
+	},
+	sizes: {
+		md: { minW: 'fit-content', w: 0, h: 0, px: 0, py: 0 }
+	},
+	variants: {
+		solid: () => ({
+			bg: 'none',
+			_active: false,
+			_hover: false,
+		}),
+	}
+};
+
 // declare custom theme
 const themes = extendTheme({
-	components: { Tabs, Textarea },
+	components: { Tabs, Textarea, Divider, Button },
 	colors: { dark },
 	config,
 	styles,
